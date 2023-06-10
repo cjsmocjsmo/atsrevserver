3358a76ca9603f511789c94369076ae8fd798d79
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-06-10 04:41:34 -0700

    test

--- src/func.rs
+++ src/func.rs
@@ -19,17 +19,17 @@ async fn allests() -> impl Responder {
 #[post("/insert_rev")]
 async fn insert_review(info: web::Json<atstypes::QInfo>) -> impl Responder {
     error!(target: "atsrevserver", "insert_review: {:?}", info);
-    let acctid = server_functions::create_account(info.email.clone());
-    error!(target: "atsrevserver", "insert_review: acctid: {:?}", acctid);
+    // let acctid = server_functions::create_account(info.email.clone());
+    // error!(target: "atsrevserver", "insert_review: acctid: {:?}", acctid);
     let db = Database::open_file("/home/pipi/atsrevserver/ats.db").expect("Could not open db file");
     let revscoll = db.collection("reviews");
     revscoll
         .insert_one(atstypes::IInfo {
-            acctid: acctid.clone(),
+            // acctid: acctid.clone(),
             name: info.name.clone(),
-            email: info.email.clone(),
-            stars: info.stars.clone(),
-            review: info.review.clone(),
+            // email: info.email.clone(),
+            // stars: info.stars.clone(),
+            // review: info.review.clone(),
         })
         .unwrap();
 
